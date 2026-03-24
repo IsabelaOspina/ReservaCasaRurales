1dbb40909cc21bd5d281468b93642575efaa7a19
IsabelaOspina <isabela.ospinab@uqvirtual.edu.co>
2026-03-24 10:42:26 -0500

    se creo el repositorio

---
+++ src/main/java/org/example/reservacasarurales/ReservaCasaRuralesApplication.java
@@ -0,0 +1,13 @@
+package org.example.reservacasarurales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaCasaRuralesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservaCasaRuralesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/reservacasarurales/ReservaCasaRuralesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.reservacasarurales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaCasaRuralesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
